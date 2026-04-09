09e2f67fecfce3a1be7a771e6d00444987b20ca9
Anelize Nardelli <anelizenardelli.tech@gmail.com>
2026-04-09 12:18:39 -0300

    initial commit: cria o projeto mvc e o .gitignore.

---
+++ dpsn-gestao-documentos-nauticos/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace dpsn_gestao_documentos_nauticos.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
